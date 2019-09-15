8ba725bc4e0ce96e72203d0584fbfd3b578ed2a6
indragunawanping <indragunawanxiaopingping@gmail.com>
2019-09-16 00:41:27 +0700

    Fourth Remove

--- week-04/src/app/places/places.model.ts
+++
@@ -1,9 +0,0 @@
-export class model_Places{
-    constructor(
-        public id: string,
-        public title: string,
-        public description: string,
-        public imgUrl: string,
-        public price: number
-    ) {}
-}
