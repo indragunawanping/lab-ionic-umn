2df266695dcba351da9c3f55ecc8d588ca9cda10
indragunawanping <indragunawanxiaopingping@gmail.com>
2019-10-06 08:54:26 +0700

    Eighth Commit

---
+++ week-08-uts/src/app/venue/venue.model.ts
@@ -0,0 +1,9 @@
+export class Venues{
+    constructor(
+       public id: string,
+       public name: string,
+       public imageUrl: string,
+       public city: string,
+       public type: string,
+    ){}
+}
