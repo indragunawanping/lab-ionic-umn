5840040d705ab54bc911fea7c3fe6af2a2bf1864
indragunawanping <indragunawanxiaopingping@gmail.com>
2019-10-16 11:12:52 +0700

    Delete week-08-uts

--- week-08-uts/src/app/venue/venue.model.ts
+++
@@ -1,9 +0,0 @@
-export class Venues{
-    constructor(
-       public id: string,
-       public name: string,
-       public imageUrl: string,
-       public city: string,
-       public type: string,
-    ){}
-}
