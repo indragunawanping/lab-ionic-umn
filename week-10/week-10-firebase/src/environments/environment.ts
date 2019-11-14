dfb559181968641dfa685f61d699f7774c11d4f7
Indra Gunawan <indragunawanxiaopingping@gmmail.com>
2019-11-14 08:30:46 +0700

    Eleventh Commit

---
+++ week-10/week-10-firebase/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAfLoCtEvmwzwjnTtEIr6aeChxBP1TaW20",
+    authDomain: "mobile2-week10-c2dbb.firebaseapp.com",
+    databaseURL: "https://mobile2-week10-c2dbb.firebaseio.com",
+    projectId: "mobile2-week10-c2dbb",
+    storageBucket: "mobile2-week10-c2dbb.appspot.com",
+    messagingSenderId: "857453706796",
+    appId: "1:857453706796:web:09ef7703e8933eca0b9b89",
+    measurementId: "G-HW8K5RC4F5"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
---
+++ week-10/week-10-php-mysql/src/app/home/new-booking/new-booking.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { BookingsService } from '../bookings.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-booking',
+  templateUrl: './new-booking.page.html',
+  styleUrls: ['./new-booking.page.scss'],
+})
+export class NewBookingPage implements OnInit {
+
+  constructor(private modalCtrl: ModalController, private bookingsSvc: BookingsService) { }
+
+
+  ngOnInit() {
+  }
+
+  closeModal(){
+    this.modalCtrl.dismiss(null, 'cancel');
+  }
+
+  addNewBooking(f: NgForm){
+    this.bookingsSvc.insertBooking({
+      'booking_name': f.value.bookingName,
+      'topic': f.value.topic,
+      'details': f.value.detalils,
+      'booking_date': f.value.bookingDate,
+      'start_hour': f.value.startHour,
+      'end_hour': f.value.endHour,
+      'creator': f.value.creator,
+    })
+  }
+
+}
